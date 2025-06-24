b1f7a21409fda25ad2eafd3c1f52ec7832bd7e2a
BijetaSah <bijetasah@30gmail.com>
2025-06-24 21:18:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/product" element={<Product />}></Route>
+          <Route path="/pricing" element={<Pricing />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
